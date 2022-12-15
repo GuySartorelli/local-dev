ff1e111a552eb0fe43d31c44b0bdfbe5d10825f1
Guy Sartorelli <guy.sartorelli@silverstripe.com>
2022-12-16 10:14:59 +1300

    List of bugs to fix

---
+++ dev-tools/current-sites/current-sites.js
@@ -0,0 +1,9 @@
+const data = [
+  {
+    "Name": "core-build-failure",
+    "URL": "http://core-build-failure_03.local/",
+    "Mailhog": "http://core-build-failure_03.local:8025",
+    "DB Port": "3303",
+    "Web IP": "10.0.3.50"
+  }
+];
