9461d95a7fce3984f82f1e80100ee3f1159f2c9c
Guy Sartorelli <guy.sartorelli@silverstripe.com>
2022-12-22 15:15:43 +1300

    Add command to set git remotes

---
+++ dev-tools/src/Command/CLI/GitSetRemotes.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace DevTools\Command\CLI;
+
+use DevTools\Command\BaseCommand;
+use Gitonomy\Git\Repository;
+use LogicException;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\Filesystem\Path;
+
+/**
+ * Based on https://gist.github.com/maxime-rainville/0e2cc280cc9d2e014a21b55a192076d9
+ */
+class GitSetRemotes extends BaseCommand
+{
+    protected static $defaultName = 'cli:git-set-remotes';
+
+    protected static $defaultDescription = 'Set the various development remotes in the current git environment.';
+
+    protected static bool $hasEnvironment = false;
+
+    /**
+     * @inheritDoc
+     */
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        /** @var SymfonyStyle $io */
+        $io = $this->getVar('io');
+        $gitRepo = new Repository(Path::canonicalize(getcwd() ?: './'));
+        $ccAccount = 'git@github.com:creative-commoners/';
+        $securityAccount = 'git@github.com:silverstripe-security/';
+        $prefixAndOrgRegex = '#^(?>git@github\.com:|https://github\.com/).*/#';
+
+        $io->writeln(self::STEP_STYLE . 'Checking out all the remotes</>');
+
+        $originUrl = trim($gitRepo->run('remote', ['get-url', 'origin']));
+
+        // Validate origin URL
+        if (!preg_match($prefixAndOrgRegex, $originUrl)) {
+            throw new LogicException("Origin $originUrl does not appear to be valid");
+        }
+
+        // Add cc and security remotes
+        $ccRemote = preg_replace($prefixAndOrgRegex, $ccAccount, $originUrl);
+        $securityRemote = preg_replace($prefixAndOrgRegex, $securityAccount, $originUrl);
+        $gitRepo->run('remote', ['add', 'cc', $ccRemote]);
+        $gitRepo->run('remote', ['add', 'security', $securityRemote]);
+
+        // Rename origin
+        if ($input->getOption('rename-origin')) {
+            $gitRepo->run('remote', ['rename', 'origin', 'orig']);
+        }
+
+        // Fetch
+        if ($input->getOption('fetch')) {
+            $gitRepo->run('fetch', ['--all']);
+        }
+
+        $successMsg = 'Remotes added';
+        if ($input->getOption('fetch')) {
+            $successMsg .= ' and fetched';
+        }
+        $io->success($successMsg);
+        return Command::SUCCESS;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    protected function configure(): void
+    {
+        $this->setAliases(['git-set-remotes']);
+
+        $desc = static::$defaultDescription;
+        $this->setHelp($desc);
+        $this->addOption(
+            'rename-origin',
+            null,
+            InputOption::VALUE_NEGATABLE,
+            'Rename the "origin" remote to "orig"',
+            true
+        );
+        $this->addOption(
+            'fetch',
+            null,
+            InputOption::VALUE_NEGATABLE,
+            'Run git fetch after defining remotes',
+            true
+        );
+    }
+}
